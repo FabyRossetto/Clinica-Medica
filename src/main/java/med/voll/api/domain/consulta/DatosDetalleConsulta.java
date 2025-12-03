0a31e8c771b2d863ec91373f3a4d9a64cf90c01b
Fabiana Rossetto <fabyross09@gmail.com>
2025-12-03 17:46:17 -0300

    le agregue el motivo de cancelamiento a los datos del detalle consulta

--- src/main/java/med/voll/api/domain/consulta/DatosDetalleConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DatosDetalleConsulta.java
@@ -2,8 +2,8 @@ package med.voll.api.domain.consulta;
 
 import java.time.LocalDateTime;
 
-public record DatosDetalleConsulta(Long id, Long idPaciente, Long idMedico, LocalDateTime fecha) {
+public record DatosDetalleConsulta(Long id, Long idPaciente, Long idMedico, LocalDateTime fecha,MotivoCancelamiento motivoCancelamiento) {
     public DatosDetalleConsulta(Consulta consulta) {
-        this(consulta.getId(),consulta.getPaciente().getId(),consulta.getMedico().getId(),consulta.getFecha());
+        this(consulta.getId(),consulta.getPaciente().getId(),consulta.getMedico().getId(),consulta.getFecha(),consulta.getMotivoCancelamiento());
     }
 }
